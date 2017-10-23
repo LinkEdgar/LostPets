15bafe11347bb4cc7fdfe22d580e0025674b5dbc
Linkedgar <Zenith@Ee0530>
2017-10-23 17:23:30 -0400

    Added a MainActivity class to hold a fragment of the two main activities in the app - adding pets to the database -searching for pets in the database EnterLost pet is the add pet fragment -adding a view pager to handle fragment swiping
    Added a test UI for adding pets into the database
    
    Added test code using picasso to add an image to the first login screen
    
    Added test code to the sign in button

---
+++ app/src/main/java/com/example/enduser/lostpets/EnterLostPetFragment.java
@@ -0,0 +1,26 @@
+package com.example.enduser.lostpets;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by EndUser on 10/22/2017.
+ */
+
+public class EnterLostPetFragment extends Fragment {
+    //default constructor
+   public EnterLostPetFragment(){
+
+   }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.enter_pet,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/enduser/lostpets/FragmentAdapter.java
@@ -0,0 +1,28 @@
+package com.example.enduser.lostpets;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by EndUser on 10/23/2017.
+ */
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    public FragmentAdapter(FragmentManager manager){
+        super((manager));
+    }
+    @Override
+    public Fragment getItem(int position) {
+        if(position == 0){
+            return new EnterLostPetFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 1;
+    }
+}
