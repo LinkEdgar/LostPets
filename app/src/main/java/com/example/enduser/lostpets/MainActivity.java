5f8709fa06ed64c7e672597a8d7386a539e92a51
Linkedgar <Zenith@Ee0530>
2017-10-23 17:34:58 -0400

    Added a MainActivity class to hold a fragment of the two main activities in the app - adding pets to the database -searching for pets in the database EnterLost pet is the add pet fragment -adding a view pager to handle fragment swiping
    Added a test UI for adding pets into the database
    
    Added test code using picasso to add an image to the first login screen
    
    Added test code to the sign in button

---
+++ app/src/main/java/com/example/enduser/lostpets/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.enduser.lostpets;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by EndUser on 10/23/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout);
+        //sets up adapter and viewpager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+    }
+}
