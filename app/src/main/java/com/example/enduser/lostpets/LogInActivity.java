edea2d199164f0c0b97aef33b882b5e661dcad17
LinkEdgar <Zenith@Ee0530>
2017-10-26 01:45:46 -0400

    On start code to check if the user is already signed in

--- app/src/main/java/com/example/enduser/lostpets/LogInActivity.java
+++ app/src/main/java/com/example/enduser/lostpets/LogInActivity.java
@@ -8,16 +8,28 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 public class LogInActivity extends AppCompatActivity {
     private Button getStartedButton;
     private Button signUpButton;
     private ImageView introImage;
-
+    //auth used to check if the user is already signed in
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Since this is the on start activity this code decides whether or not the user is sign in
+        // if they are then the main activity will show, otherwise the app will run from the sign in screen
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null){
+            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+
         setContentView(R.layout.activity_log_in);
         introImage = (ImageView) findViewById(R.id.intro_image);
         getStartedButton = (Button) findViewById(R.id.bt_getting_started);
