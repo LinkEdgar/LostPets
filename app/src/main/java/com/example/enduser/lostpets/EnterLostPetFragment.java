76da4b5b723513edf38b32ea265faacca8b647cb
LinkEdgar <Zenith@Ee0530>
2017-11-01 15:46:05 -0400

    added changes done to EdgarDev branch such as the PetEntry UI

--- app/src/main/java/com/example/enduser/lostpets/EnterLostPetFragment.java
+++ app/src/main/java/com/example/enduser/lostpets/EnterLostPetFragment.java
@@ -5,23 +5,64 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by EndUser on 10/22/2017.
  */
 
-public class EnterLostPetFragment extends Fragment {
+public class EnterLostPetFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    private Spinner mGenderSpinner;
     //default constructor
-   public EnterLostPetFragment(){
+    public EnterLostPetFragment(){
 
-   }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         //TODO Add spinner logic for the gender
-        return inflater.inflate(R.layout.enter_pet,container,false);
+        View root_view = inflater.inflate(R.layout.enter_pet,container,false);
+        setHasOptionsMenu(true);
+        //gender spinner code
+        mGenderSpinner = (Spinner) root_view.findViewById(R.id.gender_spinner);
+        String[] genderValues = {"Unknown", "Male", "Female"};
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(getContext(),android.R.layout.simple_spinner_item, genderValues);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        mGenderSpinner.setAdapter(adapter);
+        return root_view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //TODO--> add logic for selected gender
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.add_pet_item){
+            Toast.makeText(getContext(), "Yeet Sauce", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
