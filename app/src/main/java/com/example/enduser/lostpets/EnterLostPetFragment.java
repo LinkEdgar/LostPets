5a0e0b777b64af5cf1713c5a1d124fc69b3d8975
Edgar <edgar.r.link@gmail.com>
2017-11-27 02:00:51 -0500

    Added Firebase storage dependency

--- app/src/main/java/com/example/enduser/lostpets/EnterLostPetFragment.java
+++ app/src/main/java/com/example/enduser/lostpets/EnterLostPetFragment.java
@@ -31,6 +31,7 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -39,6 +40,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 import java.io.InputStream;
