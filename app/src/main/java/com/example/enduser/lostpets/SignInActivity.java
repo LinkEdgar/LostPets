30749e6c4e3d904f417aad587722e7c9345a2aed
Linkedgar <Zenith@Ee0530>
2017-12-08 23:05:45 -0500

    added the latest firebase dependencies. Fixed the google sign in option which relies on SH-1 fingerprint unique to each testing developing computer.

--- app/src/main/java/com/example/enduser/lostpets/SignInActivity.java
+++ app/src/main/java/com/example/enduser/lostpets/SignInActivity.java
@@ -12,17 +12,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -38,7 +44,7 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
     private DatabaseReference mDatabaseRef;
     //Google sign in variables
     private SignInButton googleSignIn;
-    GoogleApiClient mGoogleApiClient;
+    private GoogleSignInClient mGoogleSignInClient;
     private final static int RC_SIGN_IN = 9001;
 
 
@@ -46,7 +52,6 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        mAuth = FirebaseAuth.getInstance();
         //assigning views
         mUsername = (EditText) findViewById(R.id.et_username);
         mPassword = (EditText) findViewById(R.id.et_password);
@@ -65,6 +70,7 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
         // Sign in button code
         SetupSignIn();
         SetupRegistration();
+        mAuth = FirebaseAuth.getInstance();
     }
     //ensure app preserves the data the user input into the fields
     @Override
@@ -91,26 +97,22 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
 
     @Override
     public void onClick(View v) {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent,RC_SIGN_IN);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
     //TODO fix google sign in
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == RC_SIGN_IN){
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-            //TODO DEVELOPER ERROR in google sign in
-            Log.e("YEET 2",""+result.getStatus().toString());
-        }
-    }
-    private  void handleSignInResult(GoogleSignInResult result){
-        //TODO -->find way to add users to the database since google sign won't provide this. --> check firebase
-        if(result.isSuccess()){
-            //get the account for use with this
-            GoogleSignInAccount account = result.getSignInAccount();
-            signInSuccess();
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            }
+            catch (ApiException e){
+                Log.w("onActivityResult", "Google sign in falied", e);
+            }
         }
     }
 
@@ -122,14 +124,36 @@ public class SignInActivity extends AppCompatActivity implements GoogleApiClient
         Intent intent = new Intent(SignInActivity.this, MainActivity.class);
         startActivity(intent);
     }
-    public void SetupGoogleSignIn(){
+    private void SetupGoogleSignIn(){
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail().build();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this,this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
         googleSignIn = (SignInButton) findViewById(R.id.google_sign_in);
         googleSignIn.setOnClickListener(this);
+
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
+        Log.d("Auth with google", "firebaseAuthWithGoole:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(),null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if( task.isSuccessful()){
+                            Log.d("AuthWithGoogle", "Signin Success");
+                            FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                            //switches classes via an intent 
+                            signInSuccess();
+                        }
+                        else{
+                            Log.d("AuthWithGoogle", "Signin failure");
+                            Toast.makeText(SignInActivity.this, "Google Sign-in failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
     public void SetupSignIn(){
         signIn.setOnClickListener(new View.OnClickListener() {
