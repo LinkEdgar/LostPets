b954db58ccc9cfdc48532dd619d59d4fa560541e
Linkedgar <Zenith@Ee0530>
2018-01-03 03:10:58 -0500

    1) removed no history since it causes problems with image select 2) added navigation bar and a button on click listener to open it

--- app/src/main/java/com/example/enduser/lostpets/MainActivity.java
+++ app/src/main/java/com/example/enduser/lostpets/MainActivity.java
@@ -2,15 +2,24 @@ package com.example.enduser.lostpets;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -25,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView mAppName;
     private ImageButton mSubmitPet;
 
+    //Navigation
+    private DrawerLayout mDrawer;
+    private NavigationView mNavigationView;
+    private ImageButton mHomeButtonToggle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +51,23 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.fragment_tab_layout);
         tabLayout.setupWithViewPager(viewPager);
 
+        //Navigation stuff
+        //TODO--> find out what's wrong with the navigation bar since it doesn't close when swiped
+        ListView optionsList = (ListView)findViewById(R.id.navigation_list_view);
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        mAdapter.add("My Pets");
+        mAdapter.add("Messages");
+        mAdapter.add("Notifications");
+        mAdapter.add("Settings");
+        optionsList.setAdapter(mAdapter);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mHomeButtonToggle = (ImageButton) findViewById(R.id.navigation);
+        mHomeButtonToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawer.openDrawer(GravityCompat.START);
+            }
+        });
 
         //Toolbar stuff
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
